d9ef8df4cfedb304f100ea7fe4e17eb3cae03ef1
Chanjung Kim <chanjungskim@gmail.com>
2017-08-28 16:48:55 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,57 @@
+package com.korean.fourthproject;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
+    EditText editText;
+    ArrayList<String> itemList;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] items={""};
+        itemList=new ArrayList<String>(Arrays.asList(items));
+        adapter=new ArrayAdapter<String>(this,R.layout.list_item,R.id.txtview,itemList);
+        ListView listV=(ListView)findViewById(R.id.list);
+        listV.setAdapter(adapter);
+
+        //editText=(EditText)findViewById(R.id.txtInput);
+        Button btAdd=(Button)findViewById(R.id.btAdd);
+
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            int i = 0;
+            @Override
+            public void onClick(View v) {
+                String newItem="";
+
+                // add new item to arraylist
+                itemList.add(newItem);
+                if(i == 0){
+                    findViewById(R.id.list_item).setBackgroundColor(Color.GRAY);
+                    i=1;
+                }else{
+                    findViewById(R.id.list_item).setBackgroundColor(Color.RED);
+                    i=0;
+                }
+                // notify listview of data changed
+                adapter.notifyDataSetChanged();
+            }
+
+        });
+
+    }
+
+}
